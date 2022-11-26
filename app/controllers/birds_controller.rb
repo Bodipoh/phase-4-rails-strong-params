0c93260115e1b50389b5e4db2a91d5fccff99bd4
Bodipoh <berylodipoh@gmail.com>
2022-11-26 23:22:10 +0300

    Completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
 
+  
   # GET /birds
   def index
     birds = Bird.all
@@ -8,9 +9,17 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+     bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
 
   # GET /birds/:id
   def show
